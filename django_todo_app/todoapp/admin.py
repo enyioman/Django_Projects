062cf8223448f0a4ef20ac7edf4a07c49b5b69e3
Enyioma Nwadibia <fynewily@gmail.com>
2023-09-15 23:34:32 +0100

    features addition

--- django_todo_app/todoapp/admin.py
+++ django_todo_app/todoapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Todoapp
+from .models import Task
 
 # Register your models here.
-admin.site.register(Todoapp)
+admin.site.register(Task)
+
--- django_todo_app/todoapp/models.py
+++ django_todo_app/todoapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Todoapp(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     due_date = models.DateField()
